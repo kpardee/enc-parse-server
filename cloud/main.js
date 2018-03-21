440292adff66ffd4c53a7773c9da5ac0d1d3be9a
Kenneth Pardee <kenneth.pardee@vercadia.com>
2018-03-20 18:11:08 -0700

    dev

--- cloud/main.js
+++ cloud/main.js
@@ -1,9 +1,9 @@
 var encOutput = require('./output.js');
 
 Parse.Cloud.define('hello', function(req, res) {
-    var outputJSON = encOutput.methodGetOutputJSON('objectIdGetObject');
+    var outputJSON = encOutput.methodGetOutputJSON('hello');
     outputJSON.output.message = 'hello from Encardia';
     outputJSON = encOutput.setResponseStatus(outputJSON, 0, 'complete');
-    res.success(output);
+    res.success(outputJSON);
 
 });
